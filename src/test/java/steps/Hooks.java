aef8a1ae0a4a6ae1baeb3145d95bd3a752fd46fb
vinvyach <vinvyach@yandex.ru>
2021-03-24 02:02:25 +0700

    small refactor

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -12,29 +12,12 @@ public class Hooks {
     BasicPage basicPage = new BasicPage();
     RegistrationPage registrationPage = new RegistrationPage();
 
-//    @Before("@MainPagePopUp")
-//    public void closeMainPagePopUp(){
-//       open("https://marketplace-test.sberbank-service.ru/");
-//        closeCookiesPopup();
-//    }
-//
-//    @Before("@Registration")
-//    public void closeRegistrationPopUp(){
-//        open("https://marketplace-test.sberbank-service.ru/registration");
-//        closeCookiesPopup();
-//    }
 
-//    public void closeCookiesPopup(){
-//        if (basicPage.checkTextExist("IT Услуга использует файлы cookies")){
-//            basicPage.clickButtonSpan("OK");
-//        }
-//
-//        if (registrationPage.checkTextExist("IT Услуга использует файлы cookies")){
-//            registrationPage.clickButtonSpan("OK");
-//        }
-//
-//    }
 
+    /**
+     * resize браузера здесь, если раскоментировать то бразуер улетит в стратосферу, нет
+     * на самом деле он улетит на второй экран, если он у вас конечно есть!
+     */
 
     @Before
     public void resizeBrowser() {
@@ -43,5 +26,31 @@ public class Hooks {
         Configuration.browserSize = "1920x1080";
 
     }
+
+
+    @Before
+    public void closeMainPagePopUp(){
+       open("https://marketplace-test.sberbank-service.ru/");
+        closeCookiesPopup();
+    }
+
+    @Before("@Registration")
+    public void closeRegistrationPopUp(){
+        open("https://marketplace-test.sberbank-service.ru/registration");
+        closeCookiesPopup();
+    }
+
+    public void closeCookiesPopup(){
+        if (basicPage.checkTextExist("IT Услуга использует файлы cookies")){
+            basicPage.clickButtonSpan("OK");
+        }
+
+        if (registrationPage.checkTextExist("IT Услуга использует файлы cookies")){
+            registrationPage.clickButtonSpan("OK");
+        }
+
+    }
+
+
 
 }
